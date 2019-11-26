6eecd1a62b68a8bbf90b0a2b78714075b5ad3ab6
Sylvainboyer <contact@boyer.earth>
2019-11-26 12:43:06 +0100

    Bug correction. 'yeswiki-farm-prefix' is taken into account when deleting a wiki.

--- handlers/page/__deletepage.php
+++ handlers/page/__deletepage.php
@@ -1,20 +1,21 @@
 <?php
 // Verification de securite
 if (!defined("WIKINI_VERSION")) {
-    die("acc&egrave;s direct interdit");
+	die("acc&egrave;s direct interdit");
 }
 $type = $this->GetTripleValue($this->GetPageTag(), 'http://outils-reseaux.org/_vocabulary/type', '', '');
 if ($type == 'fiche_bazar' && $_GET['confirme'] == 'oui' && ($this->UserIsOwner() || $this->UserIsAdmin())) {
-    $tab_valeurs = baz_valeurs_fiche($this->GetPageTag());
-    if (isset($tab_valeurs["bf_dossier-wiki"]) && !empty($tab_valeurs["bf_dossier-wiki"])) {
-        $src = realpath(getcwd().'/'.(!empty($GLOBALS['wiki']->config['yeswiki-farm-root-folder']) ? $GLOBALS['wiki']->config['yeswiki-farm-root-folder'] : '.').'/'.$tab_valeurs["bf_dossier-wiki"]);
-        if (is_dir($src)) {
-            // supprimer le wiki
-            rrmdir($src);
-            // supprime les tables mysql
-            $prefix = 'yeswiki_'.str_replace('-', '_', $tab_valeurs["bf_dossier-wiki"]).'__';
-            $query = 'DROP TABLE `'.$prefix.'acls`, `'.$prefix.'links`, `'.$prefix.'nature`, `'.$prefix.'pages`, `'.$prefix.'referrers`, `'.$prefix.'triples`, `'.$prefix.'users`;';
-            $GLOBALS['wiki']->Query($query);
-        }
-    }
+	$tab_valeurs = baz_valeurs_fiche($this->GetPageTag());
+	if (isset($tab_valeurs["bf_dossier-wiki"]) && !empty($tab_valeurs["bf_dossier-wiki"])) {
+		$src = realpath(getcwd().'/'.(!empty($GLOBALS['wiki']->config['yeswiki-farm-root-folder']) ? $GLOBALS['wiki']->config['yeswiki-farm-root-folder'] : '.').'/'.$tab_valeurs["bf_dossier-wiki"]);
+		if (is_dir($src)) {
+			// supprimer le wiki
+			rrmdir($src);
+			// supprime les tables mysql
+			$prefix = $GLOBALS['wiki']->config['yeswiki-farm-prefix'];
+			$prefix .= str_replace('-', '_', $tab_valeurs["bf_dossier-wiki"]).'__';
+			$query = 'DROP TABLE `'.$prefix.'acls`, `'.$prefix.'links`, `'.$prefix.'nature`, `'.$prefix.'pages`, `'.$prefix.'referrers`, `'.$prefix.'triples`, `'.$prefix.'users`;';
+			$GLOBALS['wiki']->Query($query);
+		}
+	}
 }
