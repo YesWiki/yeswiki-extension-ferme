cf346779bf9d1cf0582f81bf09cdc4ba7939166c
srosset81 <srosset81@gmail.com>
2020-10-06 10:37:00 +0200

    Adapt extension to Bazar refactoring

--- handlers/page/__deletepage.php
+++ handlers/page/__deletepage.php
@@ -7,7 +7,7 @@ if (!defined("WIKINI_VERSION")) {
 initFarmConfig();
 $type = $this->GetTripleValue($this->GetPageTag(), 'http://outils-reseaux.org/_vocabulary/type', '', '');
 if ($type == 'fiche_bazar' && $_GET['confirme'] == 'oui' && ($this->UserIsOwner() || $this->UserIsAdmin())) {
-	$tab_valeurs = baz_valeurs_fiche($this->GetPageTag());
+	$tab_valeurs = $GLOBALS['bazarFiche']->getOne($this->GetPageTag());
 	if (isset($tab_valeurs["bf_dossier-wiki"]) && !empty($tab_valeurs["bf_dossier-wiki"])) {
 		$src = realpath(getcwd().'/'.(!empty($GLOBALS['wiki']->config['yeswiki-farm-root-folder']) ? $GLOBALS['wiki']->config['yeswiki-farm-root-folder'] : '.').'/'.$tab_valeurs["bf_dossier-wiki"]);
 		if (is_dir($src)) {
