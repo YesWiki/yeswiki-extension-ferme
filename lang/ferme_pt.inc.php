6e774f7dcb080927e8a5adbdb99f38360f246eb7
sruffin19 <83073936+sruffin19@users.noreply.github.com>
2021-04-26 21:43:34 +0200

    Create ferme_pt.inc.php

---
+++ lang/ferme_pt.inc.php
@@ -0,0 +1,45 @@
+<?php
+/*vim: set expandtab tabstop=4 shiftwidth=4: */
+// +------------------------------------------------------------------------------------------------------+
+// | PHP version 5                                                                                        |
+// +------------------------------------------------------------------------------------------------------+
+// | Copyright (C) 2016 Outils-Réseaux (accueil@outils-reseaux.org)                                       |
+// +------------------------------------------------------------------------------------------------------+
+// | This library is free software; you can redistribute it and/or                                        |
+// | modify it under the terms of the GNU Lesser General Public                                           |
+// | License as published by the Free Software Foundation; either                                         |
+// | version 2.1 of the License, or (at your option) any later version.                                   |
+// |                                                                                                      |
+// | This library is distributed in the hope that it will be useful,                                      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of                                       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                                    |
+// | Lesser General Public License for more details.                                                      |
+// |                                                                                                      |
+// | You should have received a copy of the GNU Lesser General Public                                     |
+// | License along with this library; if not, write to the Free Software                                  |
+// | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                            |
+// +------------------------------------------------------------------------------------------------------+
+//
+/**
+* Fichier de traduction en francais de l'extension Ferme
+*
+*@package       ferme
+*@author        Florian Schmitt <mrflos@gmail.com>
+*@copyright     2016 Outils-Réseaux
+*/
+
+$GLOBALS['translations'] = array_merge(
+	$GLOBALS['translations'],
+	array(
+	'FERME_IMPORT' => 'Importar',
+	'FERME_GENERATE_MODEL_FROM_URL' => 'Gerar um modelo a partir de um endereço URL',
+	'FERME_URL_IMPORT_INFO' => 'Digite o url de qualquer página do yeswiki ou sua pasta',
+	'FERME_ALL_WIKIS_ADMIN' => 'Administração de todos os wikis',
+	'FERME_UPDATING' => 'Atualização de',
+	'FERME_WIKI' => 'O wiki',
+	'FERME_UPDATED' => 'foi atualizado',
+	'FERME_FILE' => 'Arquivo',
+	'FERME_NOT_FOUND' => 'não encontrado',
+	'FERME_ADMIN_REQUIRED' => 'Você deve fazer parte do grupo @admins para administrar wikis',
+	)
+);
